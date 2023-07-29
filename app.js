4964fa779a64466d723757c8819f05f51a6711ad
ariel.olin@apiiro.com <114854641+arielolin@users.noreply.github.com>
2023-07-29 03:09:42 +0300

    fix

--- app.js
+++ app.js
@@ -29,11 +29,7 @@ const app = new App({
   })
 })
 
-// Optional: Get & log the authenticated app's name
-const { data } = await app.octokit.request('/app')
 
-// Read more about custom logging: https://github.com/octokit/core.js#logging
-app.octokit.log.debug(`Authenticated as '${data.name}'`)
 
 // Subscribe to the "pull_request.opened" webhook event
 app.webhooks.on('pull_request.opened', async ({ octokit, payload }) => {
