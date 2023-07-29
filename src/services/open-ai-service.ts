add5820b6703c296ddd9eae05308981f92c0da98
ariel.olin@apiiro.com <114854641+arielolin@users.noreply.github.com>
2023-07-29 21:48:53 +0300

    fix

--- src/services/open-ai-service.ts
+++ src/services/open-ai-service.ts
@@ -31,7 +31,7 @@ export default class OpenAIService implements IOpenAIService {
   };
 
   explainCode = async ({ code }: { code: string }) => {
-    const { data } = await this.api.post("/chat/completions", {
+    const data = await this.api.post("/chat/completions", {
       messages: [
         {
           role: "system",
@@ -49,6 +49,7 @@ export default class OpenAIService implements IOpenAIService {
       ],
       model: "gpt-3.5-turbo",
     });
+    console.log(data);
     return JSON.parse(data.choices?.[0].message.content);
   };
 }
