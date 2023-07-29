67d6992f003e18259e9734404812e9d4479316d9
ariel.olin@apiiro.com <114854641+arielolin@users.noreply.github.com>
2023-07-29 17:04:11 +0300

    add prettier

--- src/agents/gh-app.ts
+++ src/agents/gh-app.ts
@@ -1,72 +1,81 @@
-
 import fs from "fs";
-import { App } from "octokit";
+import { App as Ocktokit } from "octokit";
 
 class GitHubApp {
-    private appId: string;
-    private privateKey: string;
-    private secret: string;
-    private app: App;
-    private messageForNewPRs: string;
+  private privateKeyPath = process.env.PRIVATE_KEY_PATH as string;
+  private webhookSecret = process.env.WEBHOOK_SECRET;
+  private messagePath = "./message.md";
+  readonly messageForNewPRs: string;
+  readonly appId = process.env.APP_ID;
+  readonly privateKey = fs.readFileSync(this.privateKeyPath, "utf8");
+  private app: typeof Ocktokit;
 
-    constructor(appId: string, privateKey: string, secret: string, messagePath: string) {
-        this.appId = appId;
-        this.privateKey = privateKey;
-        this.secret = secret;
-        this.messageForNewPRs = fs.readFileSync(messagePath, "utf8");
-
-        this.app = new App({
-            appId: this.appId,
-            privateKey: this.privateKey,
-            webhooks: {
-                secret: this.secret,
-            },
-        });
-
-        this.initializeWebhooks();
+  constructor() {
+    if (
+      this.appId === undefined ||
+      this.privateKeyPath === undefined ||
+      this.webhookSecret === undefined
+    ) {
+      throw new Error("Missing required environment variable. See README.md");
     }
 
-    private initializeWebhooks(): void {
-        this.app.webhooks.onAny(({ id, name, payload }) => {
-            console.log(name, "event received");
-        });
+    this.messageForNewPRs = fs.readFileSync(this.messagePath, "utf8");
 
-        this.app.webhooks.on("pull_request.opened", async ({ octokit, payload }) => {
-            console.log(
-                `Received a pull request event for #${payload.pull_request.number}`,
+    this.app = new Ocktokit({
+      appId: this.appId,
+      privateKey: this.privateKey,
+      webhooks: {
+        secret: this.webhookSecret,
+      },
+    });
+
+    this.initializeWebhooks();
+  }
+
+  private initializeWebhooks(): void {
+    this.app.webhooks.onAny(({ id, name, payload }) => {
+      console.log(name, "event received");
+    });
+
+    this.app.webhooks.on(
+      "pull_request.opened",
+      async ({ octokit, payload }) => {
+        console.log(
+          `Received a pull request event for #${payload.pull_request.number}`,
+        );
+
+        try {
+          await octokit.rest.issues.createComment({
+            owner: payload.repository.owner.login,
+            repo: payload.repository.name,
+            issue_number: payload.pull_request.number,
+            body: this.messageForNewPRs,
+          });
+        } catch (error) {
+          if (error.response) {
+            console.error(
+              `Error! Status: ${error.response.status}. Message: ${error.response.data.message}`,
             );
+          } else {
+            console.error(error);
+          }
+        }
+      },
+    );
 
-            try {
-                await octokit.rest.issues.createComment({
-                    owner: payload.repository.owner.login,
-                    repo: payload.repository.name,
-                    issue_number: payload.pull_request.number,
-                    body: this.messageForNewPRs,
-                });
-            } catch (error) {
-                if (error.response) {
-                    console.error(
-                        `Error! Status: ${error.response.status}. Message: ${error.response.data.message}`,
-                    );
-                } else {
-                    console.error(error);
-                }
-            }
-        });
+    this.app.webhooks.onError((error) => {
+      console.log(error);
+      if (error.name === "AggregateError") {
+        console.log(`Error processing request: ${error.event}`);
+      } else {
+        console.log(error);
+      }
+    });
+  }
 
-        this.app.webhooks.onError((error) => {
-            console.log(error);
-            if (error.name === "AggregateError") {
-                console.log(`Error processing request: ${error.event}`);
-            } else {
-                console.log(error);
-            }
-        });
-    }
-
-    public getWebhooks() {
-        return this.app.webhooks;
-    }
+  public getWebhooks() {
+    return this.app.webhooks;
+  }
 }
 
 export default GitHubApp;
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,11 @@
 import dotenv from "dotenv";
-import fs from "fs";
 import http from "http";
 import { createNodeMiddleware } from "@octokit/webhooks";
 import GitHubApp from './agents/gh-app';
 
 dotenv.config();
 
-const appId = process.env.APP_ID;
-const privateKeyPath = process.env.PRIVATE_KEY_PATH as string;
-const privateKey = fs.readFileSync(privateKeyPath, "utf8");
-const secret = process.env.WEBHOOK_SECRET;
-const messagePath = "./message.md";
-
-if (
-    appId === undefined ||
-    privateKeyPath === undefined ||
-    secret === undefined
-) {
-  throw new Error("Missing required environment variable. See README.md");
-}
-
-const ghApp = new GitHubApp(appId, privateKey, secret, messagePath);
-
+const ghApp = new GitHubApp();
 const port = process.env.PORT || 3000;
 const path = "/api/webhook";
 const localWebhookUrl = `http://localhost:${port}${path}`;
