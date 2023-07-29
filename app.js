15e85dfacd07cf0e0245d793726ebde12728a322
ariel.olin@apiiro.com <114854641+arielolin@users.noreply.github.com>
2023-07-29 03:47:54 +0300

    new

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import fs from 'fs'
 import http from 'http'
-import { Octokit, App } from 'octokit'
+import {  App } from 'octokit'
 import { createNodeMiddleware } from '@octokit/webhooks'
 
 // Load environment variables from .env file
@@ -12,7 +12,7 @@ const appId = process.env.APP_ID
 const privateKeyPath = process.env.PRIVATE_KEY_PATH
 const privateKey = fs.readFileSync(privateKeyPath, 'utf8')
 const secret = process.env.WEBHOOK_SECRET
-const enterpriseHostname = process.env.ENTERPRISE_HOSTNAME
+
 const messageForNewPRs = fs.readFileSync('./message.md', 'utf8')
 
 // Create an authenticated Octokit client authenticated as a GitHub App
@@ -22,20 +22,10 @@ const app = new App({
   webhooks: {
     secret
   },
-  ...(enterpriseHostname && {
-    Octokit: Octokit.defaults({
-      baseUrl: `https://${enterpriseHostname}/api/v3`
-    })
-  })
 })
 
-// Optional: Get & log the authenticated app's name
-const { data } = await app.octokit.request('/app')
 
-// Read more about custom logging: https://github.com/octokit/core.js#logging
-app.octokit.log.debug(`Authenticated as '${data.name}'`)
 
-// Subscribe to the "pull_request.opened" webhook event
 app.webhooks.on('pull_request.opened', async ({ octokit, payload }) => {
   console.log(`Received a pull request event for #${payload.pull_request.number}`)
   try {
@@ -69,7 +59,7 @@ const port = process.env.PORT || 3000
 const path = '/api/webhook'
 const localWebhookUrl = `http://localhost:${port}${path}`
 
-
+// See https://github.com/octokit/webhooks.js/#createnodemiddleware for all options
 const middleware = createNodeMiddleware(app.webhooks, { path })
 
 http.createServer(middleware).listen(port, () => {
