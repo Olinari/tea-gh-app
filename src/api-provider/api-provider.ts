d6195fbb39f65794167b238cad3fb3faa50fb648
ariel.olin@apiiro.com <114854641+arielolin@users.noreply.github.com>
2023-07-29 20:16:25 +0300

    fix

--- src/api-provider/api-provider.ts
+++ src/api-provider/api-provider.ts
@@ -18,6 +18,7 @@ export default class ApiProvider implements IApiProvider {
     baseURL: string;
     authorization: string;
   }) {
+    console.log(baseURL, authorization);
     this.client = axios.create({
       baseURL,
       headers: {
